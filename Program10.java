6e5c3df45f18d0a0d6d025d7a80f32ac69a49aa4
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-11 09:17:54 +0530

    Prime Number

---
+++ Program10.java
@@ -0,0 +1,36 @@
+
+//     Prime Number
+
+import java.util.*;
+
+public class Program10 {
+
+    static boolean checkPrimeNumber( int num ){
+
+        int count = 0;
+
+        for( int i = 1; i < num/2; i++){
+
+            if( num % i == 0){
+                count++;
+            }
+        }
+
+        if(count == 1){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(checkPrimeNumber( num ));
+        
+    }
+    
+}
